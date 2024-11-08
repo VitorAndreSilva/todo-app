0af2fbb544cfbdf2dd3a88ed16c1f3694eb7eff5
VitorAndreSilva <alunovitorandre@gmail.com>
2024-11-08 16:57:59 -0300

    1ª aula de React Native

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>About Us!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems : "center"
+    }
+})
